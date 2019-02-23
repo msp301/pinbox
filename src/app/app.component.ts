fea78c4e78642e9ce23be3ccf21f8205832f52a3
Martin Pritchard <martin@martinpritchard.com>
2019-02-23 15:24:22 +0000

    Insert mock data for emails and labels
    - Display user labels within the side menu as loaded from the backend
    - Display some email content via mocked data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,43 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'pinbox';
+  title = 'Pinbox';
+
+  labels: EmailLabel[] = [
+    {
+        id: 1,
+        name: 'Test'
+    },
+    {
+        id: 2,
+        name: 'Thing'
+    },
+    {
+        id: 3,
+        name: 'Another thing here'
+    }
+  ];
+
+  email: Email = {
+    id: 999,
+    epoch: 1550930545657,
+    recipient: 'Test <me@test.com>',
+    sender: 'Jeff <jeff@news.co.uk>',
+    subject: 'Latest news in',
+    snippet: 'Not much has happened, it is all old news',
+  };
+}
+
+interface Email {
+  id: number;
+  epoch: number;
+  recipient: string;
+  sender: string;
+  subject: string;
+  snippet: string;
+}
+
+interface EmailLabel {
+  id: number;
+  name: string;
 }
