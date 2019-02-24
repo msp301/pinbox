0dbafdec02e70b7c36e11145b11427489b9e496a
Martin Pritchard <martin@martinpritchard.com>
2019-02-24 14:18:30 +0000

    Mockup API responses
    Fake some JSON responses for testing with the client.

--- server/pinbox-server.go
+++ server/pinbox-server.go
@@ -7,6 +7,14 @@ import (
 	"github.com/gorilla/mux"
 )
 
+func getLabels(writer http.ResponseWriter, req *http.Request) {
+	writer.Write([]byte(`[ { "id": 765, "name": "Beep" } ]`))
+}
+
+func getMessages(writer http.ResponseWriter, req *http.Request) {
+	writer.Write([]byte(`[ { "id": 999, "epoch": 1550930545657, "recipient": "Test <hello@test.com>", "sender": "API <api@server.net>", "subject": "Hello", "snippet": "Test email and stuff"  } ]`))
+}
+
 func handler(writer http.ResponseWriter, req *http.Request) {
 	writer.Write([]byte("Hello World!\n"))
 	return
@@ -17,6 +25,8 @@ func main() {
 	router := mux.NewRouter()
 
 	router.HandleFunc("/", handler)
+	router.HandleFunc("/api/labels", getLabels)
+	router.HandleFunc("/api/messages", getMessages)
 
 	log.Fatal(http.ListenAndServe(":8000", router))
 }
