be58a2d4f9006092c6c7ace2760026bdd14574d3
Martin Pritchard <martin@martinpritchard.com>
2019-03-09 11:38:09 +0000

    First pass on bundling inbox threads
    Bundling is only required in the main inbox which makes it different
    from standard labels.
    
    Provide an alternate API path specifically to provide inbox label bundling.

--- server/pinbox-server.go
+++ server/pinbox-server.go
@@ -3,11 +3,13 @@ package main
 import (
 	"encoding/base64"
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 	"net/url"
 	"os"
 	"path/filepath"
+	"time"
 
 	"github.com/gorilla/mux"
 	"github.com/jhillyerd/enmime"
@@ -39,6 +41,11 @@ type ourThread struct {
 	OldestDate int64        `json:"oldestDate"`
 	Authors    []string     `json:"authors"`
 	Messages   []ourMessage `json:"messages"`
+}
+
+type ourBundle struct {
+	ID      string       `json:"id"`
+	Threads []*ourThread `json:"threads"`
 }
 
 func openIndexDatabase(path string) *notmuch.DB {
@@ -138,44 +145,55 @@ func getMessage(writer http.ResponseWriter, req *http.Request, db *notmuch.DB) {
 	msg.Close()
 }
 
-func getMessages(writer http.ResponseWriter, req *http.Request, db *notmuch.DB, threads *notmuch.Threads) {
+func toOurThread(thr *notmuch.Thread) ourThread {
+	_, authors := thr.Authors()
+
+	var messages []ourMessage
+	msg := notmuch.Message{}
+	msgs := thr.TopLevelMessages()
+	for msgs.Next(&msg) {
+		var files []string
+		file := ""
+		filenames := msg.Filenames()
+		for filenames.Next(&file) {
+			files = append(files, file)
+		}
+
+		message := ourMessage{
+			ID:    msg.ID(),
+			Epoch: msg.Date().Unix(),
+			Files: files,
+		}
+
+		messages = append(messages, message)
+	}
+	msgs.Close()
+
+	res := ourThread{
+		ID:         thr.ID(),
+		Subject:    thr.Subject(),
+		NewestDate: thr.NewestDate().Unix(),
+		OldestDate: thr.OldestDate().Unix(),
+		Authors:    authors,
+		Messages:   messages,
+	}
+
+	return res
+}
+
+func toOurThreads(threads *notmuch.Threads) []ourThread {
 	var payload []ourThread
 	thr := notmuch.Thread{}
 	for threads.Next(&thr) {
-		_, authors := thr.Authors()
-
-		var messages []ourMessage
-		msg := notmuch.Message{}
-		msgs := thr.TopLevelMessages()
-		for msgs.Next(&msg) {
-			var files []string
-			file := ""
-			filenames := msg.Filenames()
-			for filenames.Next(&file) {
-				files = append(files, file)
-			}
-
-			message := ourMessage{
-				ID:    msg.ID(),
-				Epoch: msg.Date().Unix(),
-				Files: files,
-			}
-
-			messages = append(messages, message)
-		}
-		msgs.Close()
-
-		res := ourThread{
-			ID:         thr.ID(),
-			Subject:    thr.Subject(),
-			NewestDate: thr.NewestDate().Unix(),
-			OldestDate: thr.OldestDate().Unix(),
-			Authors:    authors,
-			Messages:   messages,
-		}
+		res := toOurThread(&thr)
 		payload = append(payload, res)
 	}
 
+	return payload
+}
+
+func getMessages(writer http.ResponseWriter, req *http.Request, db *notmuch.DB, threads *notmuch.Threads) {
+	payload := toOurThreads(threads)
 	content, err := json.Marshal(payload)
 
 	if err != nil {
@@ -204,6 +222,72 @@ func main() {
 	router := mux.NewRouter()
 
 	router.UseEncodedPath()
+
+	router.HandleFunc("/api/inbox", func(writer http.ResponseWriter, req *http.Request) {
+		query := db.NewQuery("tag:inbox and not tag:attachment")
+		threads, err := query.Threads()
+
+		labels := []string{"attachment"}
+		bundles := make(map[string]*ourBundle)
+		for _, label := range labels {
+			bundle := db.NewQuery("tag:" + label)
+			res, err := bundle.Threads()
+
+			if err != nil {
+				log.Println(fmt.Sprintf("Error with bundle '%s': %s", label, err))
+				continue
+			}
+
+			bundled := make([]*ourThread, 0)
+			var latestDate time.Time
+			thread := notmuch.Thread{}
+			for res.Next(&thread) {
+				date := thread.NewestDate()
+
+				if date.Year() > latestDate.Year() && date.Month() > latestDate.Month() {
+					latestDate = date
+				}
+
+				thr := toOurThread(&thread)
+				bundled = append(bundled, &thr)
+			}
+
+			month := fmt.Sprintf("%d %d", latestDate.Month(), latestDate.Year())
+			bundles[month] = &ourBundle{ID: label, Threads: bundled}
+		}
+
+		inbox := make([]interface{}, 0)
+		prevMonth := ""
+		thread := notmuch.Thread{}
+		for threads.Next(&thread) {
+			date := thread.NewestDate()
+			month := fmt.Sprintf("%d %d", date.Month(), date.Year())
+
+			if month != prevMonth {
+				if bundles[prevMonth] != nil {
+					inbox = append(inbox, bundles[prevMonth])
+				}
+			}
+
+			thr := toOurThread(&thread)
+			inbox = append(inbox, &thr)
+			prevMonth = month
+		}
+
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
+		content, err := json.Marshal(inbox)
+		if err != nil {
+			log.Println("Failed to convert to JSON", err)
+			return
+		}
+
+		handler(content, writer)
+		threads.Close()
+	})
 
 	router.HandleFunc("/api/labels", func(writer http.ResponseWriter, req *http.Request) {
 		getLabels(writer, req, db)
