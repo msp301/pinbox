f229eb4a9d5ecbd53095de1e7fce5c8fc7a76097
Shaun Miles <shaun.miles@sintelasystems.com>
2019-06-21 14:00:49 +0100

    Move HTTP request handling logic away from the main file to make endpoints readable

--- server/cmd/pinbox-server/main.go
+++ server/cmd/pinbox-server/main.go
@@ -1,86 +1,15 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
-	"net/url"
 	"os"
 
 	"github.com/gorilla/mux"
 
 	"github.com/msp301/pinbox-server"
 )
-
-func getLabels(writer http.ResponseWriter, req *http.Request, mailbox pinbox.Mailbox) {
-
-	payload, err := mailbox.Labels()
-
-	if err != nil {
-		log.Println("Failed to get labels", err)
-		return
-	}
-
-	content, err := json.Marshal(payload)
-
-	if err != nil {
-		log.Println("Failed to convert to JSON", err)
-		return
-	}
-
-	handler(content, writer)
-}
-
-func getMessage(writer http.ResponseWriter, req *http.Request, mailbox pinbox.Mailbox) {
-	vars := mux.Vars(req)
-	rawID, err := url.PathUnescape(vars["id"])
-
-	if err != nil {
-		log.Println("Failed to extract message ID from URL", err)
-		return
-	}
-
-	payload, err := mailbox.ReadMessage(rawID)
-
-	if err != nil {
-		log.Println("Failed to read message", err)
-		return
-	}
-
-	content, err := json.Marshal(payload)
-
-	if err != nil {
-		log.Println("Failed to convert to JSON", err)
-		return
-	}
-
-	handler(content, writer)
-}
-
-func getMessages(writer http.ResponseWriter, req *http.Request, mailbox pinbox.Mailbox, query string) {
-	payload, err := mailbox.Search(query)
-
-	if err != nil {
-		log.Println("Failed to get messages", err)
-		return
-	}
-
-	content, err := json.Marshal(payload)
-
-	if err != nil {
-		log.Println("Failed to convert to JSON", err)
-		return
-	}
-
-	handler(content, writer)
-}
-
-func handler(body []byte, writer http.ResponseWriter) {
-	writer.Header().Set("Content-Type", "application/json")
-	writer.Write(body)
-	return
-}
 
 func main() {
 	args := os.Args[1:]
@@ -97,69 +26,16 @@ func main() {
 	mailbox.Bundle = config.Bundle
 	mailbox.InboxLabel = config.Inbox
 
-	router := mux.NewRouter()
+	api := pinbox.CreateMailboxAPI(mailbox)
 
+	router := mux.NewRouter()
 	router.UseEncodedPath()
 
-	router.HandleFunc("/api/inbox", func(writer http.ResponseWriter, req *http.Request) {
-
-		inbox, err := mailbox.Inbox()
-
-		if err != nil {
-			log.Println(err)
-			return
-		}
-
-		content, err := json.Marshal(inbox)
-		if err != nil {
-			log.Println("Failed to convert to JSON", err)
-			return
-		}
-
-		handler(content, writer)
-	})
-
-	router.HandleFunc("/api/labels", func(writer http.ResponseWriter, req *http.Request) {
-		getLabels(writer, req, mailbox)
-	})
-
-	router.Path("/api/messages").Queries("label", "{label}").HandlerFunc(func(writer http.ResponseWriter, req *http.Request) {
-		url := req.URL
-		labels := url.Query()["label"]
-
-		var dbQuery = ""
-		for _, label := range labels {
-			if len(label) == 0 {
-				continue
-			}
-
-			if len(dbQuery) == 0 {
-				dbQuery = dbQuery + " tag:" + label
-			} else {
-				dbQuery = dbQuery + " and tag:" + label
-			}
-		}
-
-		if len(dbQuery) == 0 {
-			log.Println("No labels given")
-			return
-		}
-
-		if err != nil {
-			log.Println(err)
-			return
-		}
-
-		getMessages(writer, req, mailbox, dbQuery)
-	})
-
-	router.HandleFunc("/api/messages", func(writer http.ResponseWriter, req *http.Request) {
-		getMessages(writer, req, mailbox, "*")
-	})
-
-	router.HandleFunc("/api/messages/{id}", func(writer http.ResponseWriter, req *http.Request) {
-		getMessage(writer, req, mailbox)
-	})
+	router.HandleFunc("/api/inbox", api.GetInbox)
+	router.HandleFunc("/api/labels", api.GetLabels)
+	router.HandleFunc("/api/messages/{id}", api.HandleSingleMessage)
+	router.HandleFunc("/api/messages", api.HandleAllMessages)
+	router.Path("/api/messages").Queries("label", "{label}").HandlerFunc(api.HandleLabeledMessages)
 
 	port := fmt.Sprintf(":%d", config.Port)
 	log.Fatal(http.ListenAndServe(port, router))
--- server/mailbox.go
+++ server/mailbox.go
@@ -1,8 +1,152 @@
 package pinbox
 
+import (
+	"encoding/json"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+	"net/url"
+)
+
 type Mailbox interface {
 	Inbox() ([]interface{}, error)
 	Labels() ([]Label, error)
 	ReadMessage(id string) (MessageContent, error)
 	Search(query string) ([]Thread, error)
+}
+
+// MailboxAPI Contains logic for handling mailbox http requests
+type MailboxAPI struct {
+	mailbox Mailbox
+}
+
+func CreateMailboxAPI(mailbox Mailbox) *MailboxAPI {
+	return &MailboxAPI{
+		mailbox: mailbox,
+	}
+}
+
+func (m *MailboxAPI) GetInbox(writer http.ResponseWriter, req *http.Request) {
+
+	inbox, err := m.mailbox.Inbox()
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	content, err := json.Marshal(inbox)
+	if err != nil {
+		log.Println("Failed to convert to JSON", err)
+		http.Error(writer, "Failed to convert to JSON", http.StatusInternalServerError)
+		return
+	}
+
+	handler(content, writer)
+}
+
+func (m *MailboxAPI) GetLabels(writer http.ResponseWriter, req *http.Request) {
+
+	payload, err := m.mailbox.Labels()
+	if err != nil {
+		log.Println("Failed to get labels", err)
+		return
+	}
+
+	content, err := json.Marshal(payload)
+	if err != nil {
+		log.Println("Failed to convert to JSON", err)
+		http.Error(writer, "Failed to convert to JSON", http.StatusInternalServerError)
+		return
+	}
+
+	handler(content, writer)
+}
+
+func (m *MailboxAPI) HandleSingleMessage(writer http.ResponseWriter, req *http.Request) {
+	vars := mux.Vars(req)
+	rawID, err := url.PathUnescape(vars["id"])
+
+	if err != nil {
+		log.Println("Failed to extract message ID from URL", err)
+		http.Error(writer, "Failed to extract message ID from URL", http.StatusBadRequest)
+		return
+	}
+
+	payload, err := m.mailbox.ReadMessage(rawID)
+	if err != nil {
+		log.Println("Failed to read message", err)
+		http.Error(writer, "Failed to read message", http.StatusInternalServerError)
+		return
+	}
+
+	content, err := json.Marshal(payload)
+	if err != nil {
+		log.Println("Failed to convert to JSON", err)
+		return
+	}
+
+	handler(content, writer)
+}
+
+func (m *MailboxAPI) HandleAllMessages(writer http.ResponseWriter, req *http.Request) {
+	payload, err := m.mailbox.Search("*")
+	if err != nil {
+		log.Println("Failed to get messages", err)
+		http.Error(writer, "Failed to get messages", http.StatusInternalServerError)
+		return
+	}
+
+	content, err := json.Marshal(payload)
+	if err != nil {
+		log.Println("Failed to convert to JSON", err)
+		http.Error(writer, "Failed to convert to JSON", http.StatusInternalServerError)
+		return
+	}
+
+	handler(content, writer)
+}
+
+func (m *MailboxAPI) HandleLabeledMessages(writer http.ResponseWriter, req *http.Request) {
+	labels := req.URL.Query()["label"]
+
+	var dbQuery = ""
+	for _, label := range labels {
+		if len(label) == 0 {
+			continue
+		}
+
+		if len(dbQuery) == 0 {
+			dbQuery = dbQuery + " tag:" + label
+		} else {
+			dbQuery = dbQuery + " and tag:" + label
+		}
+	}
+
+	if len(dbQuery) == 0 {
+		log.Println("No labels given")
+		http.Error(writer, "No labels given", http.StatusBadRequest)
+		return
+	}
+
+	payload, err := m.mailbox.Search(dbQuery)
+	if err != nil {
+		log.Println("Failed to get messages", err)
+		http.Error(writer, "Failed to get messages", http.StatusInternalServerError)
+		return
+	}
+
+	content, err := json.Marshal(payload)
+	if err != nil {
+		log.Println("Failed to convert to JSON", err)
+		http.Error(writer, "Failed to convert to JSON", http.StatusInternalServerError)
+		return
+	}
+
+	handler(content, writer)
+}
+
+func handler(body []byte, writer http.ResponseWriter) {
+	writer.Header().Set("Content-Type", "application/json")
+	writer.Write(body)
+	return
 }
