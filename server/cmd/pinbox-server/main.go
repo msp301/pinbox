a57aadee4b8c9eec6284410b2076b89a62251d4b
Martin Pritchard <martin@martinpritchard.com>
2019-06-27 23:10:44 +0100

    Fix route resolution issue
    Requests to '/api/messages?label=' are not routed correctly as they're
    being caught by the generic '/api/messages/' route definition.
    
    Move the specfic path query route handler definitions to be before the
    resolution of the generic routes so that all possible routes can be
    matched.

--- server/cmd/pinbox-server/main.go
+++ server/cmd/pinbox-server/main.go
@@ -27,11 +27,12 @@ func main() {
 	router := mux.NewRouter()
 	router.UseEncodedPath()
 
+	router.Path("/api/messages").Queries("label", "{label}").HandlerFunc(api.HandleLabeledMessages)
+
 	router.HandleFunc("/api/inbox", api.GetInbox)
 	router.HandleFunc("/api/labels", api.GetLabels)
 	router.HandleFunc("/api/messages/{id}", api.HandleSingleMessage)
 	router.HandleFunc("/api/messages", api.HandleAllMessages)
-	router.Path("/api/messages").Queries("label", "{label}").HandlerFunc(api.HandleLabeledMessages)
 
 	port := fmt.Sprintf(":%d", config.Port)
 	log.Fatal(http.ListenAndServe(port, router))
