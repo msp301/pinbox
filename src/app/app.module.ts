a293dd87064cdd8a685fad506c49d9b18629feca
Martin Pritchard <martin@martinpritchard.com>
2019-07-22 22:05:06 +0100

    Install @angular/pwa
    This includes some minimal setup to start moving the application to act
    as a progressive web app.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -24,6 +24,8 @@ import { ThreadListComponent } from './thread-list/thread-list.component';
 import { InboxComponent } from './inbox/inbox.component';
 import { LabelResultsComponent } from './label-results/label-results.component';
 import { ThreadComponent } from './thread/thread.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 const appRoutes: Routes = [
   { path: '', redirectTo: 'inbox', pathMatch: 'full' },
@@ -58,6 +60,7 @@ const appRoutes: Routes = [
     MatSidenavModule,
     MatToolbarModule,
     MatIconModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
   ],
   providers: [
     MailboxService
