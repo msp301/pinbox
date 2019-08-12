7ac653d034b744008bdf74139ad1297991e60b6d
Martin Pritchard <martin@martinpritchard.com>
2019-08-12 22:03:52 +0100

    Install ngrx/effects

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -28,6 +28,8 @@ import { StoreModule } from '@ngrx/store';
 import { reducers, metaReducers } from './reducers';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from '../environments/environment';
+import { EffectsModule } from '@ngrx/effects';
+import { AppEffects } from './app.effects';
 
 const appRoutes: Routes = [
   { path: '', redirectTo: 'inbox', pathMatch: 'full' },
@@ -70,6 +72,7 @@ const appRoutes: Routes = [
       }
     }),
     StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
+    EffectsModule.forRoot([AppEffects]),
   ],
   providers: [
     MailboxService
